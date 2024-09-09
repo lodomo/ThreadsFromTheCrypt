37e32f583c3d9a41a9e091230fa1563da1763c91
Lorenzo Moon <lhaskins@pdx.edu>
2024-09-09 12:41:41 -0700

    Updating symbolic link .h file

--- thread_hash.h
+++ thread_hash.h
@@ -1,1 +1,39 @@
-/u/rchaney/Classes/cs333/Labs/Lab4/thread_hash.h
+// rchaney@pdx.edu
+
+#ifndef _THREAD_HASH_H
+# define _THREAD_HASH_H
+
+# define OPTIONS "i:o:d:hvt:n"
+
+# define MICROSECONDS_PER_SECOND 1000000.0
+
+# ifndef NICE_VALUE
+#  define NICE_VALUE 10
+# endif // NICE_VALUE
+
+// This comes from a terrific Stackover flow posting.
+// https://stackoverflow.com/questions/9907160/how-to-convert-enum-names-to-string-in-c
+# define FOREACH_ALGORITHM(ALGORITHM) \
+        ALGORITHM(DES)           \
+        ALGORITHM(NT)            \
+        ALGORITHM(MD5)           \
+        ALGORITHM(SHA256)        \
+        ALGORITHM(SHA512)        \
+        ALGORITHM(YESCRYPT)      \
+        ALGORITHM(GOST_YESCRYPT) \
+        ALGORITHM(BCRYPT)        \
+        ALGORITHM(ALGORITHM_MAX)
+
+# define GENERATE_ENUM(ENUM) ENUM,
+# define GENERATE_STRING(STRING) #STRING,
+
+typedef enum hash_algorithm_e {
+    FOREACH_ALGORITHM(GENERATE_ENUM)
+} hash_algorithm_t;
+
+static const char *algorithm_string[] = {
+    FOREACH_ALGORITHM(GENERATE_STRING)
+};
+
+
+#endif // _THREAD_HASH_H
